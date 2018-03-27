9d6f6f11e3e3a4e04ce9fa2be7d591a4a40e0f88
luoyangcheng <lyc163069@live.com>
2018-03-27 17:47:50 +0800

    再优化

--- Meizhu/_def.py
+++ Meizhu/_def.py
@@ -4,48 +4,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
-#通过xpath的显性等待方法
 def waitxp(b,xpath):
     locator = (By.XPATH, xpath)
     try:
         WebDriverWait(b, 20, 0.5).until(EC.visibility_of_element_located(locator))
-    except IOError:
-        print('仍然找不到元素')
-        return None
+    finally:
+        pass
 
-#通过id的显性等待方法
 def waitid(b,id):
     locator = (By.ID, id)
     try:
         WebDriverWait(b, 20, 0.5).until(EC.visibility_of_element_located(locator))
-    except IOError:
-        print('仍然找不到元素')
-        return None
+    finally:
+        pass
     # except TimeoutException as msg:
     #     pass
     # except NoSuchElementException as msg:
     #     pass
 
-def waitclass(b,classname):
-    locator = (By.CLASS_NAME, classname)
-    try:
-        WebDriverWait(b, 20, 0.5).until(EC.visibility_of_element_located(locator))
-    except IOError:
-        print('仍然找不到元素')
-        return None
-
-def waitname(b,name):
-    locator = (By.NAME, name)
-    try:
-        WebDriverWait(b, 20, 0.5).until(EC.visibility_of_element_located(locator))
-    except IOError:
-        print('仍然找不到元素')
-        return None
-
-def waitline(b,line):
-    locator = (By.LINK_TEXT, line)
-    try:
-        WebDriverWait(b, 20, 0.5).until(EC.visibility_of_element_located(locator))
-    except IOError:
-        print('仍然找不到元素')
-        return None
--- Meizhu/meizhu_login.py
+++ Meizhu/meizhu_login.py
@@ -1,83 +1,82 @@
 # 美住登录功能
 from selenium import webdriver
-from Meizhu._def import waitid, waitxp
+from Meizhu._def import waitid
 from openpyxl import load_workbook
 from termcolor import colored
-from openpyxl.styles import Font, colors, Alignment
+from openpyxl.styles import Font, colors
 
+class Login:
 
-def read_excel():
-    data = load_workbook('C:/Users\Administrator\luoyc\Meizhu\meizhu_testcase.xlsx')
-    sheet = data["美住登录"]
-    username = []
-    for r in range(2, sheet.max_row + 1):
-        if sheet.cell(row=r, column=1).value is None:
-            username.append("")
-        else:
-            username.append(sheet.cell(row=r, column=1).value)
+    def read_excel(self):
+        data = load_workbook('C:/Users\Administrator\luoyc\Meizhu\meizhu_testcase.xlsx')
+        sheet = data["美住登录"]
+        username = []
+        for r in range(2, sheet.max_row + 1):
+            if sheet.cell(row=r, column=1).value is None:
+                username.append("")
+            else:
+                username.append(sheet.cell(row=r, column=1).value)
 
-    passwd = []
-    for r in range(2, sheet.max_row + 1):
-        if sheet.cell(row=r, column=2).value is None:
-            passwd.append("")
-        else:
-            passwd.append(sheet.cell(row=r, column=2).value)
+        passwd = []
+        for r in range(2, sheet.max_row + 1):
+            if sheet.cell(row=r, column=2).value is None:
+                passwd.append("")
+            else:
+                passwd.append(sheet.cell(row=r, column=2).value)
 
-    tip = []
-    for r in range(2, sheet.max_row + 1):
-        if sheet.cell(row=r, column=3).value is None:
-            tip.append("")
-        else:
-            tip.append(sheet.cell(row=r, column=3).value)
-    return username, passwd, tip
+        tip = []
+        for r in range(2, sheet.max_row + 1):
+            if sheet.cell(row=r, column=3).value is None:
+                tip.append("")
+            else:
+                tip.append(sheet.cell(row=r, column=3).value)
+        return username, passwd, tip
 
+    def browser(self):
+        username, passwd, tip = self.read_excel()
+        url = "http://192.168.3.19:8090/login.html"
+        browser = webdriver.Firefox()
+        browser.implicitly_wait(3)
+        Result = []
+        for x, y, z in zip(username, passwd, tip):
+            browser.get(url)
+            waitid(browser, "requestUsername")
+            browser.find_element_by_id("requestUsername").send_keys(x)
+            waitid(browser, "requestPassword")
+            browser.find_element_by_id("requestPassword").send_keys(y)
+            waitid(browser, "requestSubmit")
+            browser.find_element_by_id("requestSubmit").click()
+            str = browser.find_element_by_id("login-tip").text
+            if str == z:
+                print(colored('测试通过', 'green'))
+                result_text = "PASS"
+                Result.append(result_text)
+            else:
+                print(colored('测试失败', 'red'))
+                result_text = "FAIL"
+                Result.append(result_text)
+        return Result
 
-def browser():
-    username, passwd, tip = read_excel()
-    url = "http://192.168.3.19:8090/login.html"
-    browser = webdriver.Firefox()
-    Result = []
-    for x, y, z in zip(username, passwd, tip):
-        browser.get(url)
-        waitid(browser, "requestUsername")
-        browser.find_element_by_id("requestUsername").send_keys(x)
-        waitid(browser, "requestPassword")
-        browser.find_element_by_id("requestPassword").send_keys(y)
-        waitid(browser, "requestSubmit")
-        browser.find_element_by_id("requestSubmit").click()
-        str = browser.find_element_by_id("login-tip").text
-        if str == z:
-            print(colored('测试通过', 'green'))
-            result_text = "PASS"
-            Result.append(result_text)
-        else:
-            print(colored('测试失败', 'red'))
-            result_text = "FAIL"
-            Result.append(result_text)
-    return Result
+    def write_excel(self):
+        Result = self.browser()
+        green = Font(color=colors.GREEN)
+        red = Font(color=colors.RED)
+        data = load_workbook('C:/Users\Administrator\luoyc\Meizhu\meizhu_testcase.xlsx')
+        sheet = data["美住登录"]
+        rows = sheet.max_row
+        cols = sheet.max_column
+        l_ = []
+        for i in range(2, rows + 1):
+            l_.append(i)
 
-
-def write_excel():
-    Result = browser()
-    green = Font(color=colors.GREEN)
-    red = Font(color=colors.RED)
-    data = load_workbook('C:/Users\Administrator\luoyc\Meizhu\meizhu_testcase.xlsx')
-    sheet = data["美住登录"]
-    rows = sheet.max_row
-    cols = sheet.max_column
-    l_ = []
-    for i in range(2, rows + 1):
-        l_.append(i)
-
-    for i, j in zip(l_, Result):
-        sheet.cell(i, cols - 2, j)
-        resu = sheet.cell(row=i, column=cols - 2)
-        if resu.value == "PASS":
-            resu.font = green
-        else:
-            resu.font = red
-    data.save('C:/Users\Administrator\luoyc\Meizhu\meizhu_testcase.xlsx')
-
+        for i, j in zip(l_, Result):
+            sheet.cell(i, cols - 2, j)
+            resu = sheet.cell(row=i, column=cols - 2)
+            if resu.value == "PASS":
+                resu.font = green
+            else:
+                resu.font = red
+        data.save('C:/Users\Administrator\luoyc\Meizhu\meizhu_testcase.xlsx')
 
 if __name__ == "__main__":
-    write_excel()
+    Login().write_excel()
