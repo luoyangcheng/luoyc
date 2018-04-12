f9fdfb26d754695fe83a57ba4f03e15e543a0780
luoyangcheng <lyc163069@live.com>
2018-04-12 11:28:19 +0800

    ++++++______

---
+++ Test/bubbleSort.py
@@ -0,0 +1,11 @@
+
+def bubbleSort(num):
+    for i in range(len(num)-1):
+        for j in range(len(num)-i-1):
+            if num[j] > num[j+1]:
+                num[j],num[j+1] = num[j+1],num[j]
+    return num
+
+nums = [5,2,45,6,8,3,1]
+
+print (bubbleSort(nums))
