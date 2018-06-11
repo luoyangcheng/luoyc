eb28c3f4b8ec7843847c74010dd12d9b9e2365df
luoyangcheng <lyc163069@live.com>
2018-06-11 17:57:07 +0800

    bs4 image

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,6 +1,24 @@
-from datetime import datetime
-a = datetime.now()
-b = a.strftime("%Y-%m-%d")
-c = a.strftime("%H:%M:%S")
-print(b)
-print(c)
+import requests
+import json
+import urllib
+
+
+def getSogouImag(path):
+
+    imgs = requests.get('https://app.bicijian.com/index.php?act=goods&op=goods_detail&goods_id=430198&key=&dis_id=')
+    f = json.loads(imgs.text)
+    vids = []
+    for i in range(len(f["datas"]["image_list"])):
+        vid = (f["datas"]["image_list"][i]["_small"])
+        vids.append(vid)
+    print(vids)
+    m = 0
+    for img_url in vids:
+        print('***** ' + str(m) + '.jpg *****' + '   Downloading...')
+        urllib.request.urlretrieve(img_url, path + str(m) + '.jpg')
+        m = m + 1
+    print('Download complete!')
+
+
+if __name__ == '__main__':
+    getSogouImag('F:/A/')
