9c7885fcff0a2bd29eee5f08e63c325244a205db
luoyangcheng <lyc163069@live.com>
2018-06-12 15:20:44 +0800

    爬虫

--- Demo/crawler_image.py
+++ Demo/crawler_image.py
@@ -1,28 +1,40 @@
 import requests
 import json
 import urllib
+import os
 
 
-def getBshijianImag(path, goodsid):
+def mkdirfile(file_path):
+    folder = os.path.exists(file_path)
+    if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
+        os.makedirs(file_path)  # makedirs 创建文件时如果路径不存在会创建这个路径
+        print("创建文件成功")
+    else:
+        print("文件已存在")
+
+
+def getBicijianImag(path, goods_id):
     try:
-        url = 'https://app.bicijian.com/index.php?act=goods&op=goods_detail&goods_id=' + str(goodsid) + '&key=&dis_id='
+        url = 'https://app.bicijian.com/index.php?act=goods&op=goods_detail&goods_id=' + str(goods_id) + '&key=&dis_id='
         str(url)
-        imgs = requests.get(url)
-        f = json.loads(imgs.text)
+        img_s = requests.get(url)
+        f = json.loads(img_s.text)
         img_urls = []
         for i in range(len(f["datas"]["image_list"])):
-            datas = (f["datas"]["image_list"][i]["_big"])
-            img_urls.append(datas)
+            data_s = (f["datas"]["image_list"][i]["_big"])
+            img_urls.append(data_s)
         m = 1
         for img_url in img_urls:
-            print(str(goodsid) + str(m) + '.jpg')
-            urllib.request.urlretrieve(img_url, path + str(goodsid) + '_' + str(m) + '.jpg')
+            print(str(goods_id) + str(m) + '.jpg')
+            urllib.request.urlretrieve(img_url, path + str(goods_id) + '_' + str(m) + '.jpg')
             m = m + 1
-        print('商品ID为：'+str(goodsid)+'的图片下载完成!')
+        print('商品ID为：' + str(goods_id) + '的图片下载完成!')
     except:
         print("该商品ID不存在或者已被删除")
 
 
 if __name__ == '__main__':
-    for goodsid in range(430195, 430199):
-        getBshijianImag('F:/A/', goodsid)
+    file = 'C:/xxoo/'
+    mkdirfile(file)
+    for goods_id in range(430195, 430199):
+        getBicijianImag(file, goods_id)
--- Demo/demo.py
+++ Demo/demo.py
@@ -1,4 +1,17 @@
+import os
 
-goodsid = 430198
-url = 'https://app.bicijian.com/index.php?act=goods&op=goods_detail&goods_id='+str(goodsid)+'&key=&dis_id='
-print(url)
+
+def mkdir(path):
+    folder = os.path.exists(path)
+
+    if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
+        os.makedirs(path)  # makedirs 创建文件时如果路径不存在会创建这个路径
+        print("---  new folder...  ---")
+        print("---  OK  ---")
+
+    else:
+        print("---  There is this folder!  ---")
+
+
+file = "C:\\xxoo"
+mkdir(file)  # 调用函数
