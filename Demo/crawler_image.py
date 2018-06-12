045aa65046445c991d9ed6f4e1523c321b7f332e
luoyangcheng <lyc163069@live.com>
2018-06-12 16:32:36 +0800

    爬虫

--- Demo/crawler_image.py
+++ Demo/crawler_image.py
@@ -1,16 +1,16 @@
 import requests
 import json
-import urllib
+import urllib.request
 import os
 
 
 def mkdirfile(file_path):
     folder = os.path.exists(file_path)
-    if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
-        os.makedirs(file_path)  # makedirs 创建文件时如果路径不存在会创建这个路径
-        print("创建文件成功")
+    if not folder:
+        os.makedirs(file_path)
+        print("File created successfully")
     else:
-        print("文件已存在")
+        print("File already exists")
 
 
 def getBicijianImag(path, goods_id):
@@ -28,13 +28,13 @@ def getBicijianImag(path, goods_id):
             print(str(goods_id) + str(m) + '.jpg')
             urllib.request.urlretrieve(img_url, path + str(goods_id) + '_' + str(m) + '.jpg')
             m = m + 1
-        print('商品ID为：' + str(goods_id) + '的图片下载完成!')
+        print('The download is complete，goods_id：【' + str(goods_id) + '】')
     except:
-        print("该商品ID不存在或者已被删除")
+        print('The commodity department exists or has been deleted, goods_id：【' + str(goods_id) + '】')
 
 
 if __name__ == '__main__':
-    file = 'C:/xxoo/'
-    mkdirfile(file)
-    for goods_id in range(430195, 430199):
-        getBicijianImag(file, goods_id)
+    mk_file_path = 'C:/BCJ_image/'
+    mkdirfile(mk_file_path)
+    for goods_id in range(430187, 430188):
+        getBicijianImag(mk_file_path, goods_id)
