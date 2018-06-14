2fef9e63b018dfdc1b106ed03d366f7dc1de1286
luoyangcheng <lyc163069@live.com>
2018-06-14 14:17:02 +0800

    爬虫

--- Demo/crawler_image.py
+++ Demo/crawler_image.py
@@ -2,15 +2,11 @@ import requests
 import json
 import urllib.request
 import os
+import time
 
 
-def mkdirfile(file_path, goods_id):
-    url = 'https://app.bicijian.com/index.php?act=goods&op=goods_detail&goods_id=' + str(goods_id) + '&key=&dis_id='
-    str(url)
-    img_s = requests.get(url)
-    f = json.loads(img_s.text)
-    e = f["code"]
-    if e == 200:
+def mkdirfile(file_path, goods_id, code):
+    if code == 200:
         folder = os.path.exists(file_path)
         if not folder:
             os.makedirs(file_path)
@@ -20,20 +16,13 @@ def mkdirfile(file_path, goods_id):
     else:
         print('The commodity department exists or has been deleted, goods_id：【' + str(goods_id) + '】')
 
-def getBicijianImag(path, goods_id):
+def getBicijianImag(path, goods_id,img_urls):
     try:
-        url = 'https://app.bicijian.com/index.php?act=goods&op=goods_detail&goods_id=' + str(goods_id) + '&key=&dis_id='
-        str(url)
-        img_s = requests.get(url)
-        f = json.loads(img_s.text)
-        img_urls = []
-        for i in range(len(f["datas"]["image_list"])):
-            data_s = (f["datas"]["image_list"][i]["_big"])
-            img_urls.append(data_s)
         m = 1
         for img_url in img_urls:
             print(str(goods_id) + str(m) + '.jpg')
             urllib.request.urlretrieve(img_url, path + str(goods_id) + '_' + str(m) + '.jpg')
+            time.sleep(1)
             m = m + 1
         print('The download is complete，goods_id：【' + str(goods_id) + '】')
     except:
@@ -41,7 +30,16 @@ def getBicijianImag(path, goods_id):
 
 
 if __name__ == '__main__':
-    for goods_id in range(430188, 430189):
-        mk_file_path = 'C:/BCJ_image/' + str(goods_id) + '/'
-        mkdirfile(mk_file_path, goods_id)
-        getBicijianImag(mk_file_path, goods_id)
+    for goods_id in range(430188, 430198):
+        url = 'https://app.bicijian.com/index.php?act=goods&op=goods_detail&goods_id=' + str(goods_id) + '&key=&dis_id='
+        img_s = requests.get(url)
+        f = json.loads(img_s.text)
+        code = f["code"]
+        name = f["datas"]["goods_info"]["goods_name"]
+        mk_file_path = 'C:/BCJ_image/' + str(name) + '/'
+        img_urls = []
+        for i in range(len(f["datas"]["image_list"])):
+            data_s = (f["datas"]["image_list"][i]["_big"])
+            img_urls.append(data_s)
+        mkdirfile(mk_file_path, goods_id,code)
+        getBicijianImag(mk_file_path, goods_id,img_urls)
