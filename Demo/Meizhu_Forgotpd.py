424bdd12e150b582fc9368e51c88f644ff834315
luoyangcheng <lyc163069@live.com>
2018-06-29 11:30:09 +0800

    meizhu_editped

--- Demo/Meizhu_Forgotpd.py
+++ Demo/Meizhu_Forgotpd.py
@@ -1,6 +1,10 @@
 import requests
 
-data = {'hotel': '277', 'price': '0.01'}
-getAccountBank_url = "http://115.29.142.212:8010/Home/Withdraw/getAccountBank"
-resp = requests.post(getAccountBank_url, data=data)
+data = {'mobile': '13480251015', 'vcode': '111111', 'areaCode': '86'}
+data2 = {'mobile': '13480251015', 'password': 'qq111111', 'areaCode': '86'}
+sendcode_url = "http://192.168.3.19:8090/Home/Public/checkCode"
+editped_url = "http://192.168.3.19:8090/Home/Public/resetPassword"
+resp = requests.post(sendcode_url, data=data)
+resp2 = requests.post(editped_url, data=data2)
 print(resp.content.decode('utf-8'))
+print(resp2.content.decode('utf-8'))
