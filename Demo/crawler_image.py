5c428c49b62cee2cd6ce50de9d280956bc3ab4be
luoyangcheng <lyc163069@live.com>
2018-06-30 23:17:33 +0800

    优化

--- Demo/crawler_image.py
+++ Demo/crawler_image.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
         try:
             star1 = input("请输入开始商品ID，如：430188（输完按回车）:")
             star = int(star1)
-            end1 = input("请输入结束商品ID,如：430199（输完按回车）:")
+            end1 = input("请输入结束商品ID, 如：430199（输完按回车）:")
             end = int(end1)
             if star < end:
                 all_img = []
