38929e7c5e88a7a0047788b11ce3791f88735a63
luoyangcheng <lyc163069@live.com>
2018-07-01 11:17:22 +0800

    优化

--- Demo/crawler_image.py
+++ Demo/crawler_image.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     while (i < 10):
         try:
             star1 = input("请输入开始商品ID，如：430188（输完按回车）:")
-            star = int(star1)
+            star = int(star1)           
             end1 = input("请输入结束商品ID, 如：430199（输完按回车）:")
             end = int(end1)
             if star < end:
