6e682166ee0a17e70bbfbf59a91b57612b1912a6
luoyangcheng <lyc163069@live.com>
2018-07-02 16:14:31 +0800

    pyqts

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,3 +1,22 @@
-a = input("a:")
-b = input("b:")
-print(a + b)
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 400, 220)
+        self.setWindowTitle('Rochan')
+        self.setWindowIcon(QIcon('\luoyc\Demo\Rochan.png'))
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
