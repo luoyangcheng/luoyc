ac2de23ae7bd3174ad39818a4d91f13bbd0743e7
luoyangcheng <lyc163069@live.com>
2018-07-06 11:51:51 +0800

    python37

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QWidget, QToolTip, QPushButton, QApplication
+from PyQt5.QtGui import QIcon, QFont
 
 
 class Example(QWidget):
