79cf23235102e729378c2030ca517a85c3c41232
luoyangcheng <lyc163069@live.com>
2018-10-18 09:12:20 +0800

    FirefoxOptions

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
+import time
 
 
-url = "http://192.168.3.19:8090/login.html"
-browser = webdriver.Firefox()
+url = "https://www.baidu.com"
+browser = webdriver.FirefoxOptions()
+browser.add_argument('-headless')
+browser = webdriver.Firefox(options=browser)
+# browser = webdriver.Firefox()
 browser.implicitly_wait(3)
 browser.get(url)
+print('1')
+time.sleep(5)
+js = 'window.open("https://www.sogou.com");'
+browser.execute_script(js)
+print('2')
+print(browser.current_window_handle)
