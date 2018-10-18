330c1c8864b8d25b75b3e8bd57485f6ecd48a409
luoyangcheng <lyc163069@live.com>
2018-10-18 17:49:06 +0800

    pytest

--- Test/Pytest/login.py
+++ Test/Pytest/login.py
@@ -1,3 +1,6 @@
 #
 def login():
-    assert 1 == 2
+    i = [0, 1, 2]
+    j = [0, 1, 3]
+    for a, b in zip(i, j):
+        assert a == b
--- Test/test_pytest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def test_ma():
-    assert 4 == 4
-
-
-if __name__ == '__main__':
-    pytest.main("-q test_pytest.py")
