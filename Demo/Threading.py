cedcfb35c442cefb3dd4b39b5a887e356d4d6201
luoyangcheng <lyc163069@live.com>
2018-11-20 15:43:59 +0800

    threading

--- Demo/Threading.py
+++ Demo/Threading.py
@@ -5,25 +5,22 @@ import datetime
 
 class login:
     def ll(mobile, password, areaCode):
-        for i in range(5):
-            data = {'mobile': mobile, 'password': password, 'areaCode': areaCode}
-            login_url = "http://192.168.3.19:8090/Home/Public/login"
-            session = requests.Session()
-            print(datetime.datetime.now())
-            resp = session.post(login_url, data)
-            print(resp.content.decode('utf-8'))
+        data = {'mobile': mobile, 'password': password, 'areaCode': areaCode}
+        login_url = "http://192.168.3.19:8090/Home/Public/login"
+        session = requests.Session()
+        print(datetime.datetime.now())
+        resp = session.post(login_url, data)
+        print(resp.content.decode('utf-8'))
 
+
+T = []
+for i in range(2):
+    t1 = threading.Thread(target=login.ll, args=('18802094078', 'qq111111', '86'))
+    T.append(t1)
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=login.ll, args=('17094101202', '111111b', '86'))
-    t2 = threading.Thread(target=login.ll, args=('18802094078', 'qq111111', '86'))
-    t3 = threading.Thread(target=login.ll, args=('13480251015', '111111b', '86'))
-    t1.setDaemon(False)
-    t2.setDaemon(False)
-    t3.setDaemon(False)
-    t1.start()
-    t2.start()
-    t3.start()
-    t1.join()
-    t2.join()
-    t3.join()
+    for i in T:
+        i.setDaemon(True)
+        i.start()
+    for j in T:
+        j.join()
