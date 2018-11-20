9a5454f99c1c51d8561ff59b605b200b71a3366b
luoyangcheng <lyc163069@live.com>
2018-11-20 10:46:24 +0800

    ..

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,22 +1,7 @@
-import requests
-import os
-import hashlib
+import pandas as pd
+import codecs
 
-# 禁用警告
-# from requests.packages.urllib3.exceptions import InsecureRequestWarning
-# requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-# # help(requests)
-# url = "https://passport.cnblogs.com/user/signin"
-# headers = {
-#     "User-Agent":
-#     "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:44.0)Gecko/20100101 Firefox/44.0"
-# }
-# r = requests.get(url, headers=headers, verify=False)
-# print(r.status_code)
-
-# cur_path = os.path.dirname(os.path.realpath(__file__))
-# print(cur_path)
-
-md5 = hashlib.md5()
-md5.update('how to use md5 in python hashlib?'.encode('utf-8'))
-print(md5.hexdigest())
+xd = pd.ExcelFile('C:\\report.xls')
+df = xd.parse()
+with codecs.open('C:\\report.html', 'w', 'utf-8') as html_file:
+    html_file.write(df.to_html(header=True, index=False))
