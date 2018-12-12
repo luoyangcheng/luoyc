1520662aac12818b4e8ebfaf8c1ed8618191e678
luoyangcheng <lyc163069@live.com>
2018-12-12 16:50:11 +0800

    data

--- Tool/mysqlAddData.py
+++ Tool/mysqlAddData.py
@@ -10,50 +10,64 @@ config = {
     'db': 'test',
     'cursorclass': pymysql.cursors.DictCursor
 }
-conn = pymysql.connect(**config)
-conn.autocommit(1)
-cursor = conn.cursor()
 
-try:
-    # 创建数据库
-    # DB_NAME = 'test'
-    # cursor.execute('DROP DATABASE IF EXISTS %s' % DB_NAME)
-    # cursor.execute('CREATE DATABASE IF NOT EXISTS %s' % DB_NAME)
-    # conn.select_db(DB_NAME)
 
-    # 创建表
-    # TABLE_NAME = 'user'
-    # cursor.execute(
-    #     'CREATE TABLE %s(id int primary key,name varchar(30))' % TABLE_NAME)
+class DATA(object):
+    try:
 
-    # 批量插入纪录
-    values = []
-    for i in range(2):
-        values.append((i, 'luoyc' + str(i)))
-    cursor.executemany('INSERT INTO user values(%s,%s)', values)
-    print(values)
+        def __init__(self):
+            self.conn = pymysql.connect(**config)
+            self.conn.autocommit(1)
+            self.cursor = self.conn.cursor()
 
-    # 查询数据条目
-    # TABLE_NAME = 'user'
-    # FIELD_NAME = 'ID'
-    # STR_NAME = '1'
-    # count = cursor.execute('SELECT * FROM %s WHERE %s = %s' % (TABLE_NAME, FIELD_NAME, STR_NAME))
-    # # 显示所有结果
-    # cds = cursor.fetchall()
-    # print('total records:', cursor.rowcount)
-    # print(cds)
+        # 创建数据库
+        def CREATE_DB(self):
+            DB_NAME = 'test'
+            self.cursor.execute('DROP DATABASE IF EXISTS %s' % DB_NAME)
+            self.cursor.execute('CREATE DATABASE IF NOT EXISTS %s' % DB_NAME)
+            self.conn.select_db(DB_NAME)
+            # 关闭游标连接
+            self.cursor.close()
+            # 关闭数据库连接
+            self.conn.close()
 
-    # 获取表名信息
-    # desc = cursor.description
-    # print("%s %s" % (desc[0][0], desc[1][0]))
+        # 创建表
+        def CREATE_TABLE(self):
+            TABLE_NAME = 'user'
+            self.cursor.execute(
+                'CREATE TABLE %s(id int primary key,name varchar(30))' %
+                TABLE_NAME)
+            self.cursor.close()
+            self.conn.close()
 
-except Exception:
-    import traceback
-    traceback.print_exc()
-    # 发生错误时会滚
-    conn.rollback()
-finally:
-    # 关闭游标连接
-    cursor.close()
-    # 关闭数据库连接
-    conn.close()
+        # 批量插入纪录
+        def ADD_ADTA(self):
+            values = []
+            for i in range(3, 4):
+                values.append((i, 'luoyc' + str(i)))
+            self.cursor.executemany('INSERT INTO user values(%s,%s)', values)
+            self.cursor.close()
+            self.conn.close()
+            print(values)
+
+        # 查询数据条目
+        def SELECT(self):
+            TABLE_NAME = 'user'
+            FIELD_NAME = 'ID'
+            STR_NAME = '1'
+            self.count = self.cursor.execute(
+                'SELECT * FROM %s WHERE %s = %s' % (TABLE_NAME, FIELD_NAME,
+                                                    STR_NAME))
+            # 显示所有结果
+            cds = self.cursor.fetchall()
+            self.cursor.close()
+            self.conn.close()
+            print('total records:', self.cursor.rowcount)
+            print(cds)
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    DATA().ADD_ADTA()
