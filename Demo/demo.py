e11c582a3250f31b3502277ae8ba846d71914eed
luoyangcheng <lyc163069@live.com>
2019-01-11 17:16:49 +0800

    ...

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,2 +1,7 @@
-import time
-time.sleep(1)
+from openpyxl import load_workbook
+
+data = load_workbook('C:\demo.xlsx')
+sheet = data['demo']
+print(sheet.cell(row=1, column=1).value)
+sheet.cell(2, 2, '写入哈哈')
+data.save('C:\demo.xlsx')
