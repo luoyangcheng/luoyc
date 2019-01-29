3e0cffe8fb428825eefa8d896f6bac0bc9721638
luoyangcheng <lyc163069@live.com>
2019-01-29 09:25:08 +0800

    ...

---
+++ Demo/demo.py
@@ -0,0 +1,15 @@
+import hashlib
+
+
+def demo(x, y):
+    x = x
+    print(x > y + 11 + (x - y - 11) * 0.032)
+
+
+def md5(data):
+    a = hashlib.md5(data.encode(encoding='UTF-8')).hexdigest()
+    print(a)
+
+
+demo(22.04, 10)
+md5("qq111111")
