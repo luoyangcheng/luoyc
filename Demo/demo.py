3e0e770071173637d7dda837c3055e02f6d26e78
luoyangcheng <lyc163069@live.com>
2019-01-30 17:19:57 +0800

    ...

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,4 +1,8 @@
 import hashlib
+import requests
+import itertools
+import json
+import datetime
 
 
 def demo(x, y):
@@ -11,5 +15,28 @@ def md5(data):
     print(a)
 
 
-demo(22.04, 10)
-md5("qq111111")
+def login():
+    mylist = ("".join(x) for x in itertools.product(
+        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        repeat=8))
+    for pwd in mylist:
+        data = {'mobile': '18802094078', 'password': pwd, 'areaCode': 86}
+        login_url = "http://192.168.3.19:8090/Home/Public/login"
+        session = requests.Session()
+        resp = session.post(login_url, data)
+        res = resp.content.decode('utf-8')
+        j = json.loads(res)
+        code = (j["status"])
+        print(pwd, code)
+        if code == 200:
+            print(code)
+            break
+        else:
+            print(code)
+            print(datetime.datetime.now())
+            break
+
+
+# demo(22.04, 10)
+# md5("qq111111")
+login()
