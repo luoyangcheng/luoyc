a79ae0b2e849c367245de9d6dade414fe296a414
luoyangcheng <lyc163069@live.com>
2019-03-28 17:00:38 +0800

    luo

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,15 +1,17 @@
-import hashlib
+import requests
 
 
-def demo(x, y):
-    x = x
-    print(x > y + 11 + (x - y - 11) * 0.032)
+def ll(mobile, password, areaCode):
+    data = {'mobile': mobile, 'password': password, 'areaCode': areaCode}
+    login_url = "http://192.168.3.19:8090/Home/Public/login"
+    session = requests.Session()
+    resp = session.post(login_url, data)
+    c = requests.cookies.RequestsCookieJar()
+    c.set('cookie-name', 'cookie-value')
+    session.cookies.update(c)
+    print(resp)
+    print(session.cookies.get_dict())
 
 
-def md5(data):
-    a = hashlib.md5(data.encode(encoding='UTF-8')).hexdigest()
-    print(a)
-
-
-demo(22.04, 10)
-md5("qq111111")
+if __name__ == '__main__':
+    ll(18802094078, 'qq111111', 86)
