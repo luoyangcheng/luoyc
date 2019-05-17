0d4c4dc0c78f50824b311108f77efaa2787035cc
luoyangcheng <lyc163069@live.com>
2019-05-17 18:07:23 +0800

    .

---
+++ Demo/demo.py
@@ -0,0 +1,28 @@
+from openpyxl import load_workbook
+
+
+def read_excel(excel_path, sheet_name, col):
+    data = load_workbook(excel_path)
+    sheet = data[sheet_name]
+    newdata = []
+    for r in range(2, sheet.max_row + 1):
+        if sheet.cell(row=r, column=col).value is None:
+            newdata.append("")
+        else:
+            newdata.append(sheet.cell(row=r, column=col).value)
+
+    return newdata
+
+
+def aa():
+    excel_path = "C:/test.xlsx"
+    a = read_excel(excel_path, 'demo', 1)
+    b = read_excel(excel_path, 'demo', 2)
+    s1 = set(a)
+    s2 = set(b)
+    s = s1.difference(s2)
+    print(s)
+
+
+if __name__ == "__main__":
+    aa()
