e55359037838ee56b70a0d86bbfc9c3e5c2f3c08
luoyangcheng <lyc163069@live.com>
2019-06-04 16:00:21 +0800

    jenkins

--- Demo/demo.py
+++ Demo/demo.py
@@ -1,15 +1,1 @@
-from appium import webdriver
-import time
-
-desired_caps = {}
-desired_caps['platformName'] = 'Android'
-desired_caps['platformVersion'] = '7.0'
-desired_caps['deviceName'] = 'Android Emulator'
-desired_caps['appPackage'] = 'com.uclbrt.qrcloud'
-desired_caps['appActivity'] = '.GuideActivity'
-
-driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
-time.sleep(3000)
-
-driver.quit()
+print("Hello,jenkins")
