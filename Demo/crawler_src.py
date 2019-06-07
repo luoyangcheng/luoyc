81f6b4b8ec196d438ea6de26931619f85747baf4
luoyangcheng <lyc163069@live.com>
2019-06-07 09:32:43 +0800

    crawler

--- Demo/crawler_src.py
+++ Demo/crawler_src.py
@@ -2,13 +2,14 @@ import requests
 import urllib
 from bs4 import BeautifulSoup
 
-url = 'https://tieba.baidu.com/p/6152244349'
+url = 'https://tieba.baidu.com/p/6149964702'
 
 soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-course = soup.find_all('img', class_="j_user_sign")
+course = soup.find_all('img', class_="BDE_Image")
 x = 0
 path = 'D:/demo/'
 for i in course:
     img_URL = i['src']
     urllib.request.urlretrieve(img_URL, path + '%s.jpg' % x)
+    x = x + 1
     print('正在下载：' + img_URL)
