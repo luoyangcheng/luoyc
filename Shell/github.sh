1d70c8b9c1d311e31d802abdec5b07f2a238f178
luoyangcheng <lyc163069@live.com>
2019-08-16 11:19:51 +0800

    TheFame

--- Shell/github.sh
+++ Shell/github.sh
@@ -2,4 +2,4 @@
 export PATH=/usr/local/bin:$PATH
 cd /data/luoyc
 git stash
-{git pull origin master} || {rm -rf ./TheFame/logs ./TheFame/.pytest_cache ./TheFame/__pycache__ ./TheFame/case}&& git pull origin master
+git pull origin master
