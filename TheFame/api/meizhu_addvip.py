8cca6c9c0b58362f582253cc64029b918e7c95c8
luoyangcheng <lyc163069@live.com>
2019-08-19 09:42:36 +0800

    TheFame

--- TheFame/api/meizhu_addvip.py
+++ TheFame/api/meizhu_addvip.py
@@ -4,19 +4,9 @@ sys.path.append('../TheFame/common/')
 import logger, Open_Excel
 
 
-def addvip(session, hotel, name, mobile, vipInfoId, gender, share, areaCode,
-           vipLevelName):
+def addvip(session, hotel, name, mobile, vipInfoId, gender, share, areaCode, vipLevelName):
     global result  # 设置全局变量
-    data = {
-        'hotel': hotel,
-        'name': name,
-        'mobile': mobile,
-        "vipInfoId": vipInfoId,
-        "gender": gender,
-        "share": share,
-        "areaCode": areaCode,
-        "vipLevelName": vipLevelName
-    }
+    data = {'hotel': hotel, 'name': name, 'mobile': mobile, "vipInfoId": vipInfoId, "gender": gender, "share": share, "areaCode": areaCode, "vipLevelName": vipLevelName}
     try:
         addvip_url = "http://www.meizhuyun.com/Home/Customer/addVip"
         result = session.post(addvip_url, data)
@@ -35,12 +25,8 @@ def test_addvip(session):
         one_data = Open_Excel.read_excel(excel_path, '添加会员', i)
         Test_data.append(one_data)
     actual = []
-    for hotel, name, mobile, vipInfoId, gender, share, areaCode, vipLevelName, expected in zip(
-            Test_data[0], Test_data[1], Test_data[2], Test_data[3],
-            Test_data[4], Test_data[5], Test_data[6], Test_data[7],
-            Test_data[8]):
-        addvip(session, hotel, name, mobile, vipInfoId, gender, share,
-               areaCode, vipLevelName)
+    for hotel, name, mobile, vipInfoId, gender, share, areaCode, vipLevelName, expected in zip(Test_data[0], Test_data[1], Test_data[2], Test_data[3], Test_data[4], Test_data[5], Test_data[6], Test_data[7], Test_data[8]):
+        addvip(session, hotel, name, mobile, vipInfoId, gender, share, areaCode, vipLevelName)
         if result == expected:
             log.info(filename + '--' + result)
             actual.append(result)
