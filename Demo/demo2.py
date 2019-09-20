200f57758b3221dfde49ca689930fd5f787daa56
luoyangcheng <lyc163069@live.com>
2019-09-20 18:03:10 +0800

    sqlite

--- Demo/demo2.py
+++ Demo/demo2.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(('127.0.0.1',5555))
+s.connect(('127.0.0.1',6666))
 print(s.recv(1024))
 data = input("请输入：")
 s.send(data.encode())
